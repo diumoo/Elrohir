272dc69eb08f89917d2154e9e691e8cd9c7f8100
chunxiao ye <yechunxiao19@gmail.com>
2014-01-15 22:47:56 +0800

    create EHAPIClient

---
+++ EHAPIClient.h
@@ -0,0 +1,50 @@
+//
+//  EHAPIClient.h
+//  Elrohir
+//
+//  Created by akron on 1/15/14.
+//  Copyright (c) 2014 Douban Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EHAPIClient : NSObject
+
+#pragma mark - login, logout and register
+- (void)loginWithUsername:(NSString *)username
+                 password:(NSString *)password
+                 callback:(void (^)(NSDictionary *))callback;
+- (void)logout;
+- (void)registerWithUsername:(NSString *)username
+                    password:(NSString *)password
+                    callback:(void (^)(NSDictionary *))callback;
+
+#pragma mark - User specified functions
+- (void)fetchUserInfoWithCallback:(void (^)(NSDictionary *))callback;
+- (void)fetchRecommendedChannelsWithLimit:(NSInteger)limit
+                                 callback:(void (^)(NSDictionary *))callback;
+- (void)fetchRecentChannelsWithCallback:(void (^)(NSDictionary *))callback;
+- (void)collectChannelWithChannelId:(NSInteger)channelId
+                           callback:(void (^)(NSDictionary *))callback;
+- (void)uncollectChannelWithChannelId:(NSInteger)channelId
+                             callback:(void (^)(NSDictionary *))callback;
+
+#pragma mark - General Channels
+- (void)fetchAppChannelsWithCallback:(void (^)(NSDictionary *))callback;
+- (void)fetchChannelsWithShowAsCategory:(BOOL)showAsCategory
+                               callback:(void (^)(NSDictionary *))callback;
+- (void)fetchHotChannelsWithStart:(NSInteger)start
+                            limit:(NSInteger)limit
+                         callback:(void (^)(NSDictionary *))callback;
+- (void)fetchUptrendingChannelsWithStart:(NSInteger)start
+                                   limit:(NSInteger)limit
+                                callback:(void (^)(NSDictionary *))callback;
+- (void)fetchChannelInfoWithChannelId:(NSInteger)channelId
+                             callback:(void (^)(NSDictionary *))callback;
+- (void)searchChannelsWithQuery:(NSString *)query
+                          start:(NSInteger)start
+                          limit:(NSInteger)limit
+                       callback:(void (^)(NSDictionary *))callback;
+
+
+@end
